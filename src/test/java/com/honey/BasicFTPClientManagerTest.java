fde2cf49f5ef91972042b5856ceea7cfdbe0f677
Honwhy Wang <honwhy.wang@gmail.com>
2019-01-14 23:37:28 +0800

    add more lines for connection pool test

--- src/test/java/com/honey/BasicFTPClientManagerTest.java
+++ src/test/java/com/honey/BasicFTPClientManagerTest.java
@@ -3,6 +3,9 @@ package com.honey;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BasicFTPClientManagerTest {
 
     @Test
@@ -13,6 +16,8 @@ public class BasicFTPClientManagerTest {
         manager.setUserName("root");
         manager.setPassword("123456");
         manager.setInitialSize(2);
+        manager.setMaxTotal(10);
+        manager.setMaxWaitMillis(50L);
         //Assert.assertTrue(manager.getNumActive() == 0);
         //Assert.assertTrue(manager.getNumIdle() == 2);
 
@@ -23,6 +28,20 @@ public class BasicFTPClientManagerTest {
         ftpClient.holdConnection(); //return to pool
         Assert.assertTrue(manager.getNumActive() == 0);
         Assert.assertTrue(manager.getNumIdle() == 2);
+        List<PooledFTPClient> pooledFTPClientList = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            pooledFTPClientList.add(manager.getFTPClient());
+        }
+        Assert.assertTrue(manager.getNumActive() == 10);
+        Assert.assertTrue(manager.getNumIdle() == 0);
+        try {
+            manager.getFTPClient(); //can not get from pool anymore
+            Assert.fail("would not get another one from pool");
+        } catch (Exception e) {
+        }
+        for (PooledFTPClient pooledFTPClient : pooledFTPClientList) {
+            pooledFTPClient.holdConnection();
+        }
         manager.close();
     }
 }
